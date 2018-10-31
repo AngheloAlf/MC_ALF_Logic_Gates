347b22aa1388991ff3fc3ea6db351f7bda1c4226
AngheloAlf10 <angheloalf95@gmail.com>
2018-10-31 09:22:13 -0300

    Cleaning some code

--- src/main/java/angheloalf/alf_logic_gates/blocks/datablock/ClockEntity.java
+++ src/main/java/angheloalf/alf_logic_gates/blocks/datablock/ClockEntity.java
@@ -10,20 +10,12 @@ import net.minecraft.util.ITickable;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
-import java.util.List;
 
 public class ClockEntity extends TileEntity implements ITickable{
-    // We only calculate this value on the client so we don't care about the server
-    // side and we also don't care about saving this value. That's why we have no
-    // readFromNBT() and writeToNBT().
     private boolean lit = false;
 
-    // Counter that is decremented faster if there are more entities in the vicinity.
-    // If it reaches negative we toggle the light
     private int counter = 0;
-
-    // To prevent counting entities every tick we delay it for 10 ticks and remember the last count we had.
-    private int delayCounter = 10;
+    // private int delayCounter = 10;
     private int lastCount = 1;
 
     public ClockEntity(){
@@ -40,25 +32,8 @@ public class ClockEntity extends TileEntity implements ITickable{
         counter -= lastCount * 4;
         if (counter <= 0) {
             markDirty();
-            // world.notifyBlockUpdate(pos, null, null, 3);
             lit = !lit;
-            counter = 400;      // This is 20 seconds. Rate increases if more mods are near
-            // world.markBlockRangeForRenderUpdate(getPos(), getPos());
-        }
-    }
-
-    private void updateCounter() {
-        // Don't count the entities every tick. That would be a bit slow.
-        delayCounter--;
-        if (delayCounter <= 0) {
-            //List<EntityLivingBase> list = world.getEntitiesWithinAABB(EntityLivingBase.class, new AxisAlignedBB(
-            //         getPos().add(-10, -10, -10), getPos().add(10, 10, 10)));
-            delayCounter = 10;
-            lastCount = 1;
-            // lastCount = list.size();
-        }
-        else{
-            lastCount = 0;
+            counter = 400;
         }
     }
 
@@ -71,7 +46,6 @@ public class ClockEntity extends TileEntity implements ITickable{
     public void readFromNBT(NBTTagCompound compound) {
         super.readFromNBT(compound);
         counter = compound.getInteger("counter");
-        delayCounter = compound.getInteger("delayCounter");
         lastCount = compound.getInteger("lastCount");
         lit = compound.getBoolean("lit");
     }
@@ -80,7 +54,6 @@ public class ClockEntity extends TileEntity implements ITickable{
     public NBTTagCompound writeToNBT(NBTTagCompound compound) {
         compound = super.writeToNBT(compound);
         compound.setInteger("counter", counter);
-        compound.setInteger("delayCounter", delayCounter);
         compound.setInteger("lastCount", lastCount);
         compound.setBoolean("lit", lit);
         return compound;
