75665d96bb2d34859806b1e1bd02fc7ab81756c7
AngheloAlf10 <angheloalf95@gmail.com>
2018-11-06 08:36:00 -0300

    Disables search bar in creative tab.

--- src/main/java/angheloalf/alf_logic_gates/creativetabs/LogicGatesTab.java
+++ src/main/java/angheloalf/alf_logic_gates/creativetabs/LogicGatesTab.java
@@ -1,15 +1,19 @@
 package angheloalf.alf_logic_gates.creativetabs;
 
-import angheloalf.alf_logic_gates.Mod_ALF_Logic_Gates;
 import angheloalf.alf_logic_gates.ModItems;
 
+import mcp.MethodsReturnNonnullByDefault;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.ItemStack;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+@MethodsReturnNonnullByDefault
 public class LogicGatesTab extends CreativeTabs {
     private boolean withSearch;
 
-    public LogicGatesTab(String name, String background, boolean withSearch){
+    public LogicGatesTab(@Nonnull String name, @Nullable String background, boolean withSearch){
         super(name);
         if(background != null){
             this.setBackgroundImageName(background);
